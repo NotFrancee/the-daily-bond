4a02051efed6941236c8f28c24a2b9c4afa6b53c
Andrea Franceschini <65458493+NotFrancee@users.noreply.github.com>
2022-07-01 10:49:06 +0200

    Initial commit

---
+++ gatsby-config.ts
@@ -0,0 +1,56 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `CHANGE THIS`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "CHANGE-THIS",
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "@chakra-ui/gatsby-plugin",
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {boolean} [isUsingColorMode=true]
+         * if false, this plugin will not use <ColorModeProvider />
+         */
+        isUsingColorMode: true,
+      },
+    },
+  ],
+};
+
+export default config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Test Site</h1>
+      <p>this is a test site</p>
+    </div>
+  );
+};
+
+export default Index;
