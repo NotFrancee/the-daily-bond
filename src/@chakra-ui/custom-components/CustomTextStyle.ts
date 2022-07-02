f4563a832b7c1d3f27008af04ac5257b82438f4b
Andrea Franceschini <andrea.franceschini03@gmail.com>
2022-07-02 18:50:14 +0200

    Created custom styles for buttons, headings and text. Implemented fonts and worked on heading font

---
+++ src/@chakra-ui/custom-components/CustomTextStyle.ts
@@ -0,0 +1,12 @@
+import { ComponentStyleConfig } from "@chakra-ui/react";
+
+const CustomTextStyle: ComponentStyleConfig = {
+  baseStyle: {
+    lineHeight: "175%",
+  },
+  variants: {
+    subheading: {},
+  },
+};
+
+export default CustomTextStyle;
--- src/components/homepage/Introduction.tsx
+++ src/components/homepage/Introduction.tsx
@@ -5,17 +5,17 @@ import StyledFlex from "../shared/StyledFlex";
 const Introduction = () => {
   return (
     <Flex px={5} py={10} direction="column" gap={5}>
-      <Heading color={"primary.highlight"}>
+      <Heading>
         Impara a gestire le tue finanze e riprendi il controllo della tua vita
       </Heading>
-      <Text>
+      <Text variant={"subheading"}>
         Tutte le risorse presenti sul sito sono gratuite e dritte al punto. Non
         vogliamo venderti niente, solo migliorare la tua gestione dei soldi per
         migliroare il tuo stile di vita
       </Text>
       {/* // cambia colore mettendo quello main  */}
-      <Image src="https://ik.imagekit.io/astrum/TheDailyBond/engineeringTeam?ik-sdk-version=javascript-1.4.3&updatedAt=1656770854737" />
-      <Button variant={"primary-btn"}>Esplora i nostri Articoli</Button>
+      <Image src="https://ik.imagekit.io/astrum/TheDailyBond/engineeringTeam" />
+      <Button>Esplora i nostri Articoli</Button>
       {/* Image of something inspiring, cartooned */}
     </Flex>
   );
