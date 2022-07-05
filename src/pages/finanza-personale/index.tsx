6c743588df8171bb52ec7848bb2f8604d0de00c2
Andrea Franceschini <andrea.franceschini03@gmail.com>
2022-07-05 18:00:30 +0200

    Technical SEO improvements

--- src/pages/finanza-personale/index.tsx
+++ src/pages/finanza-personale/index.tsx
@@ -1,10 +1,14 @@
 import { Heading } from "@chakra-ui/react";
 import React from "react";
-import { Layout } from "../../components/shared";
+import { Layout, SEO } from "../../components/shared";
 
 const index = () => {
   return (
     <Layout>
+      <SEO
+        title="Finanza Personale - The Daily Bond"
+        description="Esplora le nostre risorse sulla finanza personale!"
+      />
       <Heading as={"h1"}>Finanza Personale</Heading>
     </Layout>
   );
