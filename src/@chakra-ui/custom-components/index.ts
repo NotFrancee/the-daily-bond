aec8355a714b19de98c258ebfd5737d53676441c
Andrea Franceschini <andrea.franceschini03@gmail.com>
2022-07-09 22:44:55 +0200

    changed link default style

--- src/@chakra-ui/custom-components/index.ts
+++ src/@chakra-ui/custom-components/index.ts
@@ -2,9 +2,11 @@ import CustomButtonStyle from "./CustomButtonStyle";
 import CustomTextStyle from "./CustomTextStyle";
 import CustomHeadingStyle from "./CustomHeadingStyle";
 import SectionComponent from "./SectionComponent";
+import CustomLinkStyle from "./CustomLinkStyle";
 export {
   CustomButtonStyle,
   CustomTextStyle,
   CustomHeadingStyle,
   SectionComponent,
+  CustomLinkStyle,
 };
