ceeeef48e0cd50b580ea269efb1dba73137334e5
Andrea Franceschini <andrea.franceschini03@gmail.com>
2022-07-09 22:44:55 +0200

    changed link default style

---
+++ src/@chakra-ui/custom-components/CustomLinkStyle.ts
@@ -0,0 +1,10 @@
+import { ComponentStyleConfig } from "@chakra-ui/react";
+import { Link as GatsbyLink } from "gatsby";
+
+const CustomLinkStyle: ComponentStyleConfig = {
+  baseStyle: {
+    color: "primary.highlight",
+  },
+};
+
+export default CustomLinkStyle;
